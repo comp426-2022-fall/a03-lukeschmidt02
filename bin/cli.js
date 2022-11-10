49b6f908ce99572c8aadb7c55e37208c96d87d38
lukeschmidt02 <77812790+lukeschmidt02@users.noreply.github.com>
2022-11-10 17:28:16 -0500

    Changed some things up to get defaults working.
    Having issue running and it might be because im using a different computer as my main one broke

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,12 @@
-//#!/usr/bin/env node
+#!/usr/bin/env node
 
 import { roll } from "../lib/roll.js"
 import minimist from "minimist"
 
 const args = minimist(process.argv.slice(2));
-console.log("test");
 
-console.log(JSON.stringify(roll(args.sides, args.dice, args.rolls)));
+const sides_resp = args.sides || 6
+const dice_resp = args.dice || 2
+const rolls_resp = args.rolls || 1
+
+console.log(JSON.stringify(roll(sides_resp, dice_resp, rolls_resp)));
